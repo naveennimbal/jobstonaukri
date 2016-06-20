38387778b9af934949d5f92691689d2240dfd2d1
naveennimbal <naveen@Naveens-iMac.local>
2016-06-21 04:47:34 +0530

    desktop work of cart committing

--- public/js/functions.js
+++ public/js/functions.js
@@ -3,6 +3,25 @@
  */
 $(document).ready(function(){
 
+   // alert("dfdshf");
 
+  /*  $("#addToCart").click(function(){
+
+       // alert("dfdshf");
+        $("#resform").submit(function(){
+            console.log(this);
+            alert("for submit");
+            return false;
+        })
+    })
+    */
+
+
+    $("#resform").submit(function(){
+        console.log(this);
+        alert("for submit");
+        return false;
+    })
 
 });
+
