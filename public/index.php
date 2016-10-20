695cfacc6d69bfc28adca1541d948dccaec37d1c
Naveen Nimbal <naveen.nimbal@gmail.com>
2016-10-20 13:52:52 +0530

    paytm and banner update

--- public/index.php
+++ public/index.php
@@ -1,5 +1,33 @@
 <?php
 define("BASEURL", "http://".$_SERVER['SERVER_NAME']);
+
+define('PAYTM_ENVIRONMENT', 'PROD'); // PROD
+
+
+
+$PAYTM_DOMAIN = "pguat.paytm.com";
+if (PAYTM_ENVIRONMENT == 'PROD') {
+    $PAYTM_DOMAIN = 'secure.paytm.in';
+}
+
+if (PAYTM_ENVIRONMENT == 'PROD') {
+    $PAYTM_DOMAIN = 'secure.paytm.in';
+    define('PAYTM_MERCHANT_KEY', '7pcha8fMr1uCKRcK'); //Change this constant's value with Merchant key downloaded from portal
+    define('PAYTM_MERCHANT_MID', 'FOXIES52706480784220'); //Change this constant's value with MID (Merchant ID) received from Paytm
+    define('PAYTM_MERCHANT_WEBSITE', '3foxiesweb'); //Change this constant's value with Website name received from Paytm
+
+} else {
+    // for Testing Purposes
+    define('PAYTM_MERCHANT_KEY', 'miGmIS6dIloyT26I'); //Change this constant's value with Merchant key downloaded from portal
+    define('PAYTM_MERCHANT_MID', '3FOXIE03444669624368'); //Change this constant's value with MID (Merchant ID) received from Paytm
+    define('PAYTM_MERCHANT_WEBSITE', '3foxies'); //Change this constant's value with Website name received from Paytm
+
+}
+define('PAYTM_CALLBACK_URL',"http://".$_SERVER['SERVER_NAME']."/payment/paytmres");
+
+define('PAYTM_REFUND_URL', 'https://'.$PAYTM_DOMAIN.'/oltp/HANDLER_INTERNAL/REFUND');
+define('PAYTM_STATUS_QUERY_URL', 'https://'.$PAYTM_DOMAIN.'/oltp/HANDLER_INTERNAL/TXNSTATUS');
+define('PAYTM_TXN_URL', 'https://'.$PAYTM_DOMAIN.'/oltp-web/processTransaction');
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
